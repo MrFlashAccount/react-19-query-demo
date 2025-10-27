1ba8767fcd11d369d587af9544eec0be068407da
Sergei Garin <sergei@garin.dev>
2025-10-27 09:50:28 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,25 +10,5 @@ export default defineConfig({
   ],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        chunkFileNames: "[name].js",
-        manualChunks: {
-          shared: ["./src/components/shared/index.ts"],
-          "local-tanstack-query": [
-            "./src/components/LocalTanStackQueryTab",
-            "@tanstack/react-query-local",
-          ],
-          "tanstack-query": [
-            "./src/components/TanStackQueryTab",
-            "@tanstack/react-query",
-          ],
-          "custom-library": [
-            "./src/components/CustomLibraryTab",
-            "./src/lib/index.ts",
-          ],
-        },
-      },
-    },
   },
 });
