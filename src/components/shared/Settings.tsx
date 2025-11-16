166db970131c5db697351fa412512e56fea36773
Sergei Garin <sergikgarin@gmail.com>
2025-11-16 18:59:53 +0300

    Update Settings.tsx

--- src/components/shared/Settings.tsx
+++ src/components/shared/Settings.tsx
@@ -31,7 +31,7 @@ export function Settings({ formState, onFormStateChange }: SettingsProps) {
   return (
     <div className="flex flex-none relative">
       {/* Tooltip */}
-      <div className="absolute -top-10 left-[20%] z-50 pointer-events-none">
+      <div className="absolute -top-10 left-[10%] z-50 pointer-events-none">
         <div className="relative">
           <div className="bg-black text-white text-xs px-3 py-2 rounded-2xl [corner-shape:squircle] whitespace-nowrap shadow-lg">
             Try playing with settings!
