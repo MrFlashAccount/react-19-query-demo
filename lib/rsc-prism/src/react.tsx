53a9df44dc4e1c8b424785e643bf3d764a521189
Sergei Garin <sergei@garin.dev>
2026-02-14 17:53:00 +0300

    Add early-invalidation

--- lib/rsc-prism/src/react.tsx
+++ lib/rsc-prism/src/react.tsx
@@ -1,6 +1,13 @@
-import { createContext, use } from "react";
+import { createContext, use, useEffect, useState } from "react";
 import { fetchRSC } from "./client";
 import type { RSCTransport } from "./transport";
+
+const $$invalidations = new Set<WeakRef<() => void>>();
+export function invalidateRSC() {
+  for (const invalidation of $$invalidations) {
+    invalidation.deref()?.();
+  }
+}
 
 const RSCTransportContext = createContext<RSCTransport | null>(null);
 
@@ -17,12 +24,25 @@ export function RSCTransportProvider({
 export function rsc<Props = unknown>(
   reference: (props: Props) => React.JSX.Element | null | React.JSX.Element[],
 ) {
-  let __cacheKey: string;
-  let __cachePromise: Promise<React.ReactNode>;
+  let __cacheKey: string | undefined;
+  let __cachePromise: Promise<React.ReactNode> | undefined;
 
   return function RSCLoader(props: Props) {
     const transport = use(RSCTransportContext);
     const key = JSON.stringify(props);
+    const [, $$refresh] = useState({});
+
+    useEffect(() => {
+      const ref = new WeakRef(() => {
+        __cachePromise = undefined;
+        __cacheKey = undefined;
+        return $$refresh({});
+      });
+      $$invalidations.add(ref);
+      return () => {
+        $$invalidations.delete(ref);
+      };
+    }, []);
     if (__cacheKey === key) {
       return __cachePromise;
     }
